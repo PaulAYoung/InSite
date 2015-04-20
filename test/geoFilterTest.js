42680fe2b106e06bc30381a348b0129941e03173
Paul Young <paulandresyoung@gmail.com>
2015-04-20 13:19:33 -0700

    added some more filter tests

--- test/geoFilterTest.js
+++ test/geoFilterTest.js
@@ -22,4 +22,12 @@ describe("geoFilter", function(){
     it("should not return any items for zero max distance", function(){
         assert.equal(0, geoFilter([1, 1], items, 0).length);
     });
+
+    it("should only return item 7", function(){
+        assert.equal(7, geoFilter([-122.32779622077942, 37.89016347485787], items, 1)[0].id);
+    });
+
+    it("should work with L.LatLong objects", function(){
+        assert.equal(7, geoFilter(L.latLng([-122.32779622077942, 37.89016347485787]), items, 1)[0].id);
+    });
 });
