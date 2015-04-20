e9c534560ff8a2f8aad9389c7edd98f012a97ef0
Paul Young <paulandresyoung@gmail.com>
2015-04-19 21:25:08 -0700

    tests using zombie

---
+++ test/webtest.js
@@ -0,0 +1,18 @@
+var assert = require('chai').assert;
+var Zombie = require('zombie');
+
+
+describe('basic web tests', function(){
+    var browser = new Zombie();
+
+    before(function(done){
+        console.log(browser);
+        browser.visit("http://localhost:8080/", function(){
+            done();
+        });
+    });
+
+    it("should load the page", function(){
+        browser.assert.text("title", "InSite");
+    });
+});
