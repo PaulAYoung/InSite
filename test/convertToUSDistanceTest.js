3f0b230c1ec8991e4aa8d3a8e491206f11019b49
Sandra <sandralee08@gmail.com>
2015-05-01 22:58:30 -0700

    convertTest test 9

--- test/convertToUSDistanceTest.js
+++ test/convertToUSDistanceTest.js
@@ -10,10 +10,10 @@ describe('convertToUSDistance', function(){
         assert.equal('ft',convertToUSDistance(1).unit);
     });
 
-    // it("1001 ft (305.1048m) should return .19 mile", function(){
-    //     assert.equal(.19, convertToUSDistance(305.1048).num);
-    //     assert.equal('mi', convertToUSDistance(305.1048).unit);
-    // });
+    it("1001 ft (305.1048m) should return .19 mile", function(){
+        assert.equal(.19, convertToUSDistance(305.1048).num);
+        assert.equal('mi', convertToUSDistance(305.1048).unit);
+    });
 
     // it("1000 ft (304.8m) should return 1000 ft", function(){
     //     assert.equal(1000, convertToUSDistance(304.8).num);
