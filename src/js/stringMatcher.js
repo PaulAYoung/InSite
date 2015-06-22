44acdbe4689e0d11dc9b9a54dfd3bc9e5b367868
Paul Young <paulandresyoung@gmail.com>
2015-06-22 11:02:54 -0700

    make stringMatcher case insensitive

--- src/js/stringMatcher.js
+++ src/js/stringMatcher.js
@@ -6,13 +6,13 @@ function Matcher(tags){
      */
 
     if (typeof defaultString === 'undefined') defaultString = 'default';
-    this._tags = tags;
-    this._default = defaultString;
+    this._tags = tags.map(function(t){ return t.toLowerCase(); });
 }
 
 Matcher.prototype = {
     firstMatch: function(tags){
         // returns the first match from tags.
+        tags = tags.toLowerCase();
         for(var i=0, tag, l=this._tags.length; i<l, tag=this._tags[i]; i++){
             if (tags.indexOf(tag) !== -1){return tag;}
         }
--- test/stringMatcherTest.js
+++ test/stringMatcherTest.js
@@ -5,12 +5,16 @@ var assert = require('chai').assert;
 
 var matcher = new stringMatcher(['a', 'b', 'c'], "default");
 
-describe('DataFilter', function(){
+describe('StringMatcher', function(){
     it("should match a", function(){
         assert.equal("a", matcher.firstMatch('a'));
     });
 
     it("should return false", function(){
         assert.isFalse(matcher.firstMatch('f'));
+    });
+
+    it("should ignore case", function(){
+        assert.equal("a", matcher.firstMatch('A'));
     });
 });
